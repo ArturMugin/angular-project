74d05371e6f3515882eecc3d958dbfaa3daf79c5
ArturMugin <arturjancik@gmail.com>
2022-03-26 14:29:03 -0500

    Understanding of the ngIf
    Pushing to the branch to have a note about a Directive called ngIf that makes if/else a lot cleaner and easier

--- angular-project/src/app/servers/servers.component.ts
+++ angular-project/src/app/servers/servers.component.ts
@@ -17,6 +17,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created";
   serverName = "TestServer";
+  serverCreated = false;
 
   constructor() { 
     setTimeout(() => {
@@ -30,6 +31,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = "Server was created " + this.serverName;
+    this.serverCreated = true;
   }
   onUpdateServerName(event: Event){
     this.serverName = (<HTMLInputElement>event.target).value;
